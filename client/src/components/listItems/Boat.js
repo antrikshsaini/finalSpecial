830ddf18670d7bdcdb178e12ed56f0725ffa33fb
Antrix <sainiantriksh6@gmail.com>
2020-12-11 21:22:17 -0800

    third

--- client/src/components/listItems/Boat.js
+++ client/src/components/listItems/Boat.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react'
-import { Card, List } from 'antd'
+import React, { useState } from "react";
+import { Card, List } from "antd";
 
 // import { EditOutlined, RightOutlined } from '@ant-design/icons'
 // import UpdatePerson from '../forms/UpdatePerson'
@@ -7,63 +7,77 @@ import { Card, List } from 'antd'
 
 const getStyles = () => ({
   card: {
-    width: '500px'
-  }
-})
+    width: "500px",
+  },
+});
 
-const Boat = props => {
-  const [id] = useState(props.id)
-  const [firstName, setFirstName] = useState(props.firstName)
-  const [lastName, setLastName] = useState(props.lastName)
-  const [editMode, setEditMode] = useState(false)
+const Boat = (props) => {
+  const [id] = useState(props.id);
+  //   const [firstName, setFirstName] = useState(props.firstName)
+  //   const [lastName, setLastName] = useState(props.lastName)
+  //   const [editMode, setEditMode] = useState(false)
   // const [editRightMode, setEditRightMode] = useState(false)
-  const styles = getStyles()
+  const [year, setYear] = useState(props.year);
+  const [make, setMake] = useState(props.make);
+  const [model, setModel] = useState(props.model);
+  const [price, setPrice] = useState(props.price);
+  const [personId, setPersonId] = useState(props.personId);
+  const styles = getStyles();
 
   const fullName = () => {
-    return `${props.firstName} ${props.lastName}`
-  }
+    return `${props.year} ${props.make} ${props.model} ${props.price} ${props.personId}`;
+  };
 
   const updateStateVariable = (variable, value) => {
     switch (variable) {
-      case 'firstName':
-        setFirstName(value)
-        break
-      case 'lastName':
-        setLastName(value)
-        break
+      case "year":
+        setYear(value);
+        break;
+      case "make":
+        setMake(value);
+        break;
+      case "model":
+        setModel(value);
+        break;
+      case "price":
+        setPrice(value);
+        break;
+      case "personId":
+        setPersonId(value);
+        break;
       default:
-        break
+        break;
     }
-  }
+  };
 
-  const handleButtonClick = () => setEditMode(!editMode)
+  const handleButtonClick = () => setEditMode(!editMode);
   // const handleButtonRightClick = () => setEditRightMode(!editRightMode)
 
   return (
     <List.Item key={props.id}>
       {editMode ? (
-        <UpdatePerson
+        <UpdateBoat
           id={id}
-          firstName={firstName}
-          lastName={lastName}
+         year={year}
+         make ={make}
+         price = {price}
+         personId = {personId}
           onButtonClick={handleButtonClick}
           updateStateVariable={updateStateVariable}
         />
       ) : (
         <Card
           actions={[
-            <EditOutlined key='edit' onClick={handleButtonClick} />,
-            <RemovePerson id={id} firstName={firstName} lastName={lastName} />
+            <EditOutlined key="edit" onClick={handleButtonClick} />,
+            <RemoveBoat id={id} year={year} make={make} model={model} price={price} personId={personId} />,
           ]}
           style={styles.card}
         >
-          {fullName()}
-          <Boat  id={id} />,
-          
+         {fullName()}
         </Card>
       )}
     </List.Item>
-  )
-}
+  );
+};
 
-export default Boat
+export default Boat;
